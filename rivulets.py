1682254ea847e330649a494382266bae3b248608
eafeicke <eafeicke@indiana.edu>
2015-09-20 21:34:38 -0600

    Starting on front end

--- rivulets.py
+++ rivulets.py
@@ -2,16 +2,18 @@ import pygame
 from slope import Slope
 
 grid_size = 50
-max_resistance = 50
-rain_delta = 10
+max_resistance = 1
+rain_delta = 15
 screen_size = 500
 mod_west = 0
 mod_southwest = 2
 mod_south = 4
 mod_southeast = 2
 mod_east = 0
+begin_color = (110, 60, 0)
 
-slope = Slope(grid_size, max_resistance, rain_delta, screen_size, mod_west, mod_southwest, mod_south, mod_southeast, mod_east)
+slope = Slope(grid_size, max_resistance, rain_delta, screen_size, mod_west, \
+              mod_southwest, mod_south, mod_southeast, mod_east, begin_color)
 
 drops = 100
 for drop in range(drops):
